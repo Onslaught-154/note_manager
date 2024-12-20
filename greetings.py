2a6a8fd0270142845076a9c5562fc3093fe31f1e
Onslaught-154 <nikolaevaleks110698@gmail.com>
2024-12-20 16:40:20 +0700

    greetings.py

---
+++ greetings.py
@@ -0,0 +1,15 @@
+# Задаём переменной значение
+username = 'Alexey'
+title = 'My_first_grade'
+content = 'I_do_my_job'
+status = 'At_work'
+created_date = '20-12-2024'
+issue_date = '20-12-2024'
+
+# Выводим описание переменной
+print('Alexey:', username)
+print('My_first_grade:', title)
+print('I_do_my_job:', content)
+print('At_work:', status)
+print('20-12-2024:', created_date)
+print('20-12-2024:', issue_date)
